8b73fbaead4e0398e015a0654dee3bff66c605ed
Cyrille Meichel <cmeichel@free.fr>
2021-05-13 21:31:55 +0200

    fix carriage return

--- content.js
+++ content.js
@@ -14,20 +14,59 @@ var extractResponses = function() {
             return {ret: [
                 $(this).find('td.qcm_examen_no').text().replace(/[^\d].*/, ''),
                 $(this).find('img[alt=\'Gagné !\']').length,
-                $(this).find('td.qcm_examen_question').text().replace('<br>', "\n"),
+                '"' + $(this).find('td.qcm_examen_question').text().replaceAll('<br>', "\r").replaceAll('"', '""') + '"',
             ]};
         })
         .get();
 }
 
+function listStat() {
+    return $('form table tbody tr td table')
+    .map(function() {
+        const samples = $(this).find('tbody tr td table tbody tr').map(function(){
+            return $(this).find('td.qcm_examen_reponses').map(function() {
+                return $(this).text();
+            }).get();
+        }).get();
+
+        const links = $(this).find('tbody tr td table tbody tr a').map(function(){
+            return window.location.origin + '/' + $(this).attr('href');
+        }).get();
+
+        var s = [];
+        for (var i=0; i<samples.length/4; i++) {
+            s = s.concat({
+                date: samples[i*4],
+                score: samples[i*4+2],
+                time: samples[i*4+3],
+                link: links[i],
+            });
+        }
+        
+        return {
+            title: $(this).find('h3').text(),
+            samples: s,
+        };
+    }).get().filter(function(elt) {return elt.title});
+}
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    const ret = extractResponses();
-    ret.unshift({ret:['#;Succès;Question']})
-    console.log(ret);
-    if (msg.action === 'report_back') {
-        sendResponse(ret);
+    switch (msg.action) {
+        case 'report_back':
+            const resp = extractResponses();
+            resp.unshift({ret:['#;Succès;Question']})
+            console.log(resp);
+            sendResponse(resp);
+        case 'get_stat':
+            const stats = listStat();
+            console.log(stats);
+            sendResponse(stats);
+        default:
+            sendResponse('empty');
     }
 });
+
+
 
 
 console.log("Extractor is ready")
--- main.js
+++ main.js
@@ -3,14 +3,36 @@ document.addEventListener('DOMContentLoaded', function() {
        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {action: "report_back"}, function(response) {
                 
-                const blob = new Blob([response.map((d) => d.ret.join(';')).join("\n")], {type: "text/csv"});
+                const blob = new Blob([response.map((d) => d.ret.join(',')).join("\n")], {type: "text/csv; charset=utf-8"});
                 chrome.downloads.download({
                     url: URL.createObjectURL(blob),
                     filename: "stat.csv",
+                    headers:[
+                        {
+                            name: 'Content-Type',
+                            value: 'text/csv; charset=utf-8',
+                        }
+                    ]
                 }, cb);
             });  
         });
+
+        /*chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "get_stat"}, function(response) {
+                console.log('response', response);
+                response.forEach(function(elt) {
+                    const a = chrome.tabs.create({
+                        url: elt.link,
+                    });
+                    console.log(a);
+                });
+            });
+            console.log(tabs[0].id);
+        });*/
+
     }
     
     GrabGligli(() => window.close());
+
+    console.log('script loaded');
 });
